ed4952397963e43ea73abf2ae5e70cadf6b69161
hollisticated-horse <spam3@happyngreen.fr>
2025-10-10 17:20:57 +0200

    Speed up SVG image embedding with pooled PNG buffers

--- hayro-svg/src/image.rs
+++ hayro-svg/src/image.rs
@@ -3,10 +3,16 @@ use crate::SvgRenderer;
 use crate::mask::{ImageLuminanceMask, MaskKind};
 use base64::Engine;
 use hayro_interpret::{Device, FillRule, LumaData, Paint, PathDrawMode, RgbData};
-use image::{DynamicImage, ImageBuffer, ImageFormat};
+use image::codecs::png::PngEncoder;
+use image::{ColorType, DynamicImage, ImageBuffer, ImageEncoder, RgbImage};
 use kurbo::{Affine, Rect, Shape};
-use std::io::Cursor;
 use std::sync::Arc;
+use std::thread_local;
+
+thread_local! {
+    static PNG_BUFFER: std::cell::RefCell<Vec<u8>> = std::cell::RefCell::new(Vec::new());
+    static DATA_URI: std::cell::RefCell<String> = std::cell::RefCell::new(String::new());
+}
 
 impl<'a> SvgRenderer<'a> {
     pub(crate) fn draw_rgba_image(
@@ -27,16 +33,19 @@ impl<'a> SvgRenderer<'a> {
                     .flat_map(|(rgb, a)| [rgb[0], rgb[1], rgb[2], a])
                     .collect::<Vec<u8>>();
 
-                let image = DynamicImage::ImageRgba8(
-                    ImageBuffer::from_raw(rgb_data.width, rgb_data.height, interleaved).unwrap(),
+                self.write_image_bytes(
+                    rgb_data.width,
+                    rgb_data.height,
+                    ColorType::Rgba8,
+                    &interleaved,
+                    rgb_data.interpolate,
+                    None,
+                    transform,
                 );
-
-                self.write_image(&image, rgb_data.interpolate, None, transform);
             } else {
-                let image = DynamicImage::ImageRgb8(
-                    ImageBuffer::from_raw(rgb_data.width, rgb_data.height, rgb_data.data.clone())
-                        .unwrap(),
-                );
+                let image_buf =
+                    RgbImage::from_vec(rgb_data.width, rgb_data.height, rgb_data.data.clone())
+                        .unwrap();
 
                 let alpha = {
                     let image = DynamicImage::ImageLuma8(
@@ -57,16 +66,33 @@ impl<'a> SvgRenderer<'a> {
                 };
 
                 self.push_transparency_group_inner(1.0, Some(MaskKind::Image(Arc::new(alpha))));
-                self.write_image(&image, rgb_data.interpolate, None, transform);
+                self.write_image_bytes(
+                    image_buf.width(),
+                    image_buf.height(),
+                    ColorType::Rgb8,
+                    image_buf.as_raw(),
+                    rgb_data.interpolate,
+                    None,
+                    transform,
+                );
                 self.pop_transparency_group();
             }
         } else {
-            let image = DynamicImage::ImageRgb8(
-                ImageBuffer::from_raw(rgb_data.width, rgb_data.height, rgb_data.data.clone())
-                    .unwrap(),
+            let RgbData {
+                data,
+                width,
+                height,
+                interpolate,
+            } = rgb_data;
+            self.write_image_bytes(
+                width,
+                height,
+                ColorType::Rgb8,
+                &data,
+                interpolate,
+                None,
+                transform,
             );
-
-            self.write_image(&image, rgb_data.interpolate, None, transform);
         };
     }
 
@@ -87,11 +113,15 @@ impl<'a> SvgRenderer<'a> {
                     .flat_map(|d| if *d == 255 { color } else { [0, 0, 0, 0] })
                     .collect::<Vec<u8>>();
 
-                let image = DynamicImage::ImageRgba8(
-                    ImageBuffer::from_raw(stencil.width, stencil.height, image).unwrap(),
+                self.write_image_bytes(
+                    stencil.width,
+                    stencil.height,
+                    ColorType::Rgba8,
+                    &image,
+                    interpolate,
+                    None,
+                    transform,
                 );
-
-                self.write_image(&image, interpolate, None, transform);
             }
             Paint::Pattern(_) => {
                 let mask = {
@@ -125,18 +155,75 @@ impl<'a> SvgRenderer<'a> {
         id: Option<Id>,
         transform: Affine,
     ) {
+        match image {
+            DynamicImage::ImageRgb8(buf) => {
+                self.write_image_bytes(
+                    buf.width(),
+                    buf.height(),
+                    ColorType::Rgb8,
+                    buf.as_raw(),
+                    interpolate,
+                    id,
+                    transform,
+                );
+            }
+            DynamicImage::ImageRgba8(buf) => {
+                self.write_image_bytes(
+                    buf.width(),
+                    buf.height(),
+                    ColorType::Rgba8,
+                    buf.as_raw(),
+                    interpolate,
+                    id,
+                    transform,
+                );
+            }
+            DynamicImage::ImageLuma8(buf) => {
+                self.write_image_bytes(
+                    buf.width(),
+                    buf.height(),
+                    ColorType::L8,
+                    buf.as_raw(),
+                    interpolate,
+                    id,
+                    transform,
+                );
+            }
+            other => {
+                let rgba = other.to_rgba8();
+                self.write_image_bytes(
+                    rgba.width(),
+                    rgba.height(),
+                    ColorType::Rgba8,
+                    rgba.as_raw(),
+                    interpolate,
+                    id,
+                    transform,
+                );
+            }
+        }
+    }
+
+    pub(crate) fn write_image_bytes(
+        &mut self,
+        width: u32,
+        height: u32,
+        color: ColorType,
+        data: &[u8],
+        interpolate: bool,
+        id: Option<Id>,
+        transform: Affine,
+    ) {
         let scaling = if interpolate { "smooth" } else { "pixelated" };
-
-        let base64 = to_base64(image);
-
+        let base64 = to_base64_bytes(width, height, color, data);
         self.xml.start_element("image");
         if let Some(id) = id {
             self.xml.write_attribute("id", &id);
         }
         self.write_transform(transform);
         self.xml.write_attribute("xlink:href", &base64);
-        self.xml.write_attribute("width", &image.width());
-        self.xml.write_attribute("height", &image.height());
+        self.xml.write_attribute("width", &width);
+        self.xml.write_attribute("height", &height);
         self.xml.write_attribute("preserveAspectRatio", "none");
         self.xml
             .write_attribute("style", &format_args!("image-rendering: {scaling}"));
@@ -144,14 +231,23 @@ impl<'a> SvgRenderer<'a> {
     }
 }
 
-pub(crate) fn to_base64(image: &DynamicImage) -> String {
-    let mut png_buffer = Vec::new();
-    let mut cursor = Cursor::new(&mut png_buffer);
-    image.write_to(&mut cursor, ImageFormat::Png).unwrap();
+fn to_base64_bytes(width: u32, height: u32, color: ColorType, data: &[u8]) -> String {
+    PNG_BUFFER.with(|png| {
+        let mut png = png.borrow_mut();
+        png.clear();
+        {
+            let encoder = PngEncoder::new(&mut *png);
+            encoder
+                .write_image(data, width, height, color.into())
+                .expect("PNG encoding failed");
+        }
 
-    let mut url = "data:image/png;base64,".to_string();
-    let data = base64::engine::general_purpose::STANDARD.encode(png_buffer);
-    url.push_str(&data);
-
-    url
+        DATA_URI.with(|uri| {
+            let mut uri = uri.borrow_mut();
+            uri.clear();
+            uri.push_str("data:image/png;base64,");
+            base64::engine::general_purpose::STANDARD.encode_string(&*png, &mut uri);
+            uri.clone()
+        })
+    })
 }
